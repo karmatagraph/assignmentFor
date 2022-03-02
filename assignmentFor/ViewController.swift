8cf7a9ed3d2e4ce5c81981222b51c44d692fdce7
karma <karmatashi954@gmail.com>
2022-03-02 13:54:35 +0545

    add of navigation controller

--- assignmentFor/ViewController.swift
+++ assignmentFor/ViewController.swift
@@ -9,6 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var loginBtnLabel: UIButton!
+    
+    @IBAction func loginBtnClicked(_ sender: UIButton) {
+    }
+    
+    @IBAction func signUpBtnInvisible(_ sender: UIButton) {
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
