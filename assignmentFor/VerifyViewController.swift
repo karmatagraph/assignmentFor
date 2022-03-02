0899975ebea625e3e1d734a8a82c24587bb2d672
karma <karmatashi954@gmail.com>
2022-03-02 13:00:37 +0545

    add of signup and verify VC and design portion

---
+++ assignmentFor/VerifyViewController.swift
@@ -0,0 +1,29 @@
+//
+//  VerifyViewController.swift
+//  assignmentFor
+//
+//  Created by karma on 3/2/22.
+//
+
+import UIKit
+
+class VerifyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
